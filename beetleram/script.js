f424cbb36c7f2b3526cc25bfd96d635f775a18cd
batleram <batleram@gmail.com>
2022-05-01 13:42:47 -0400

    setup socials

--- beetleram/script.js
+++ beetleram/script.js
@@ -7,3 +7,22 @@ function handleNavigationClick() {
     }
 
 }
+
+function handleDiscordButtonClick() {
+    let dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = "Beetleram#1751";
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    setDiscordToolTip();
+}
+function setDiscordToolTip() {
+    let tooltip = document.getElementById("discord-tooltip");
+    tooltip.innerHTML = "Beetleram#1751 Copied!";
+
+}
+function resetDiscordToolTip() {
+    let tooltip = document.getElementById("discord-tooltip");
+    tooltip.innerHTML = "Copy Beetleram#1751";
+}
