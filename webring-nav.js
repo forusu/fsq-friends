8a5ed816371734763b41ed847902a53e0c20df0f
Paige <32252078+nataralis@users.noreply.github.com>
2023-02-18 20:15:01 +0100

    Update webring-nav.js Minor update to clean up a piece of code

--- webring-nav.js
+++ webring-nav.js
@@ -9,12 +9,13 @@ const createPageLinkElement = (elementText, navDirection, site) => {
 const renderPageLinkElement = async (elementText, navDirection, scriptElement) => {
     const data = await fetch("https://forusu.github.io/fsq-friends/pages.json")
         .then((response) => response.json());
-    const index = data.pages.findIndex((site) => site.link == window.location.pathname);
+    const pageList = data.pages;
+    const index = pageList.findIndex((site) => site.link == window.location.pathname);
     if (index === -1) {
         throw new Error(`Couldn't find the site with path '${window.location.pathname}' in the webring.`);
     }
-    const prevSite = data.pages[(index + data.pages.length - 1) % data.pages.length];
-    const nextSite = data.pages[(index + 1) % data.pages.length];
+    const prevSite = pageList[(index + pageList.length - 1) % pageList.length];
+    const nextSite = pageList[(index + 1) % pageList.length];
     let pageLinkElement;
     switch (navDirection) {
         case "previous":
